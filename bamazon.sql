93335a42e74a90f97289b4e2c7a5bb94422facb5
Ryan Nguyen <ryan@Ryans-MBP.hsd1.ut.comcast.net>
2019-02-05 22:56:00 -0700

    finish customer

--- bamazon.sql
+++ bamazon.sql
@@ -1,43 +1,53 @@
-DROP DATABASE IF EXISTS bamazon_DB; 
+DROP DATABASE IF EXISTS bamazon_db; 
 
-CREATE DATABASE bamazon_DB; 
+CREATE DATABASE bamazon_db; 
 
-USE bamazon_DB; 
+USE bamazon_db; 
 
 CREATE TABLE products(
-    item_id INTEGER(20) AUTO_INCREMENT NOT NULL, 
-    product_name VARCHAR(100) NOT NULL, 
-    department_name VARCHAR(100) NOT NULL, 
-    price INTEGER(20) AUTO_INCREMENT NOT NULL, 
-    stock_quality INTEGER(20) AUTO_INCREMENT NOT NULL
-)
+    itemid INTEGER AUTO_INCREMENT NOT NULL, 
+    productname VARCHAR(45) NOT NULL, 
+    departmentname VARCHAR(45) NOT NULL, 
+    price DECIMAL(10,4) NOT NULL, 
+    stockquality INTEGER(10)  NOT NULL,
+    primary key (itemid) 
+);
 
-INSERT INTO products(product_name, department_name, price, stock_quality)
-VALUES("Samsung 65inch OLED TV", "Electronics", 1400, 10);
+INSERT INTO products(productname, departmentname, price, stockquality)
+VALUES("Memory Foam Mattress", "Home Goods", 189.00, 75);
 
-INSERT INTO products(product_name, department_name, price, stock_quality)
-VALUES("Falken Tire 255/40/18", "Automotive", 150, 20);
-
-
-INSERT INTO products(product_name, department_name, price, stock_quality)
-VALUES("Blendtech Professional 8000", "Home Goods", 800, 15);
+INSERT INTO products(productname, departmentname, price, stockquality)
+VALUES("Sports Action Camera", "Electronics", 72.99, 23);
 
 
-INSERT INTO products(product_name, department_name, price, stock_quality)
-VALUES("Vitamix Commerical Blender", "Home Goods", 1040, 19);
+INSERT INTO products(productname, departmentname, price, stockquality)
+VALUES("Octopus Hoopla Bathtime", "Home Goods", 6.88, 81);
 
 
-INSERT INTO products(product_name, department_name, price, stock_quality)
-VALUES("Iphone XR 128G", "Electronics", 799, 100 );
+INSERT INTO products(productname, departmentname, price, stockquality)
+VALUES("Thermal Fleece Balaclavar", "Home Goods", 13.19, 17);
 
 
-INSERT INTO products(product_name, department_name, price, stock_quality)
-VALUES("Samsung Note 9", "Electronics", 799, 200);
+INSERT INTO products(productname, departmentname, price, stockquality)
+VALUES("Pet Training Pads", "Pets Goods", 16.99, 88 );
 
 
-INSERT INTO products(product_name, department_name, price, stock_quality)
-VALUES("Hanes Boxer Men Small", "Clothing", 5.99, 1000);
+INSERT INTO products(productname, departmentname, price, stockquality)
+VALUES("SmartBase Mattress Foundation", "Home Goods", 64.00, 43);
 
 
-INSERT INTO products(product_name, department_name, price, stock_quality)
-VALUES("Lebron Laker Jersey", "Clothing", 200, 40);
+INSERT INTO products(productname, departmentname, price, stockquality)
+VALUES("Ice Scraper", "Automotive", 6.39, 97);
+
+
+INSERT INTO products(productname, departmentname, price, stockquality)
+VALUES("Swimming Penguin", "Toys", 4.99, 55);
+
+INSERT INTO products(productname, departmentname, price, stockquality)
+VALUES("Dog Treats", "Pets Goods", 7.39, 38);
+
+INSERT INTO products(productname, departmentname, price, stockquality)
+VALUES("Headphones", "Electronics", 69.99, 15);
+
+
+SELECT * FROM products;
---
+++ bamazonCustomer.js
@@ -0,0 +1,73 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host:"localhost",
+    port:3306, 
+    user:"root", 
+    password:"Formulad1", 
+    database:"bamazon_db"
+})
+
+connection.connect(function(err){
+    if(err) throw err;
+    console.log("connection succesful!")
+    makeTable();
+})
+
+var makeTable = function(){
+    connection.query("SELECT * FROM products", function(err,res){
+        for(var i=0; i<res.length; i++){
+            console.log(res[i].itemid+" || "+res[i].productname+" || "+res[i].departmentname+" || "+res[i].price+" || "+res[i].stockquality+"\n");
+        }
+        promoptCustomer(res);
+    })
+}
+
+var promoptCustomer = function(res){
+    inquirer.prompt([{
+        type:"input", 
+        name:"choice", 
+        message:"what would you like to purhase? [Quit with Q]"
+    }]).then(function(answer){
+        var correct = false;
+        if(answer.choice.toUpperCase()=="Q"){
+            process.exit();
+        }
+        for (var i=0; i<res.length; i++){
+            if(res[i].productname==answer.choice){
+                correct=true; 
+                var product = answer.choice;
+                var id=i; 
+
+                inquierer.prompt({
+                    type:"input", 
+                    name:"quant", 
+                    message:"How many would you like to buy?",
+                    validate: function(value){
+                        if(isNaN(value)==false){
+                            return true;
+                        } else{
+                            return false;
+                        }
+                    }
+
+                }).then(function(answer){
+                    if((res[id].stockquality-answer.quant)>0){
+                        connection.query("UPDATE proucts SET stock_quality='" + (res[id].stockquality-answer.quant)+"' WHERE product_name'"+product +"'", function (err, res2){
+                            console.log("Product Bought!");
+                            makeTable();
+                        })
+                    }else {
+                        console.log("Not a valid selection!");
+                        promoptCustomer(res);
+                    }
+                })
+            }
+        }
+        if(i==res.length && correct==false){
+            console.log("Not a valid selection!");
+            promoptCustomer(res); 
+        }
+    })
+}
